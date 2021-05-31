941a9c4173c6ada356d01ba674aa92d1efd65303
kobutomo <n.tomoro13@gmail.com>
2021-05-31 21:49:23 +0900

    190 d

--- ABC190/D/main.cpp
+++ ABC190/D/main.cpp
@@ -3,11 +3,18 @@ using namespace std;
 #define endl '\n'
 
 int main() {
+  int MAX = 3000000, ans = 0;
+  long long n;
+  cin >> n;
+  for (double i = 1; i < MAX; ++i) {
+    long long a = 2 * n - i * i + i;
+    double c = a / double(2 * i);
+    if (c < 1) break;
+    if (floor(c) == c) {
+      ans++;
+      ans++;
+    }
+  }
 
-  ////////////////////////////////
-  //                            //
-  //   Write your answer here.  //
-  //                            //
-  ////////////////////////////////
-
+  cout << ans << endl;
 }
