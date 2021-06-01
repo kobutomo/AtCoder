8cc72a6b4e59d39a90a0c22dcc1d122e2869e96f
kobutomo <n.tomoro13@gmail.com>
2021-06-01 12:00:48 +0900

    189 d

--- ABC189/D/main.cpp
+++ ABC189/D/main.cpp
@@ -1,13 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define endl '\n'
+#define ll long long
 
 int main() {
-
-  ////////////////////////////////
-  //                            //
-  //   Write your answer here.  //
-  //                            //
-  ////////////////////////////////
-
+  ll n, ans = 1;
+  cin >> n;
+  vector<string> S(n);
+  for (int i = 0; i < n; ++i) {
+    cin >> S.at(i);
+  }
+  for (int i = 0; i < S.size(); ++i) {
+    string s = S[i];
+    if (s == "OR") {
+      ans += powl(2, i + 1);
+    }
+  }
+  cout << ans << endl;
 }
